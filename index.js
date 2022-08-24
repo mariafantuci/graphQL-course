477c3c9d93ec0e4b044eb6d68e57609d5d055829
mahfantuci <maria@imaginationmedia.com>
2022-08-23 21:55:58 -0300

    add the apollo basic struct

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const { ApolloServer, gql } = require('apollo-server');
 
+const typeDefs = gql`
+`;
+
+const resolvers = {
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
